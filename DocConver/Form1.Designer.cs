513723949566761645a9966a19a20f0ce13cadd5
Lin <linwenhao5>
2023-02-09 15:43:30 +0100

    Doconcert v_0.1

--- DocConver/Form1.Designer.cs
+++ DocConver/Form1.Designer.cs
@@ -176,7 +176,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Docovert";
+            this.Text = "Doconvert";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
